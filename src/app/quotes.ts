ca9508463a9281f2c933aa9aebc451b5dca6713c
VONETTA <thevonetteway@gmail.com>
2022-04-10 00:03:39 +0300

    add constructors

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,2 +1,16 @@
 export class Quotes {
+    id:number;
+    quote:string;
+    upvotes: number;
+    downvotes:number;
+
+    constructor(id: number, quote: string,upvotes: number, downvotes: number){
+        this.id=id;
+        this.upvotes=upvotes;
+        this.downvotes=downvotes
+        this.quote=quote;
+
+
+    }
+    
 }
