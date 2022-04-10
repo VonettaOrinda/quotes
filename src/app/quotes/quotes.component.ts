91e52b9b9420ece064c2776ee8aa7e919b710251
VONETTA <thevonetteway@gmail.com>
2022-04-10 18:34:45 +0300

    add quotes to the component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes'; 
+
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.scss']
 })
 export class QuotesComponent implements OnInit {
+  quotes: Quote[]=[
+
+    new Quote(
+      1."Every positive thought is a silent prayer that will change your life", 'Author: Amazing me no Movement'
+    new Date(2017,9,17)
+    ),
+    new Quote(
+      2, 'Sometimes the bad things that happen in our lives put us directly on the path to the best thing that will ever happen to us'
+    )
+  ]
 
   constructor() { }
 
